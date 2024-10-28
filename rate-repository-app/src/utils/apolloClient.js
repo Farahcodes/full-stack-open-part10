91c6e60a61ae35b89609cdb2df0d11ff8c84ba7c
farahcodes <farah.fournat@gmail.com>
2024-10-28 15:31:36 +0100

    10.11: fetching repositories with Apollo Client

--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://192.168.1.100:4000/graphql',
+    uri: 'http://192.168.1.107:4000/graphql',
     cache: new InMemoryCache(),
   });
 };
