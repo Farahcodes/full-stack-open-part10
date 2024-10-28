3745b544b85a7f46614e94d04eb48a3c2a88f2cf
farahcodes <farah.fournat@gmail.com>
2024-10-28 14:36:45 +0100

    configure GraphQL and Apollo client

--- rate-repository-api/src/index.js
+++ rate-repository-api/src/index.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import http from 'http';
 
 import logger from './utils/logger';
--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
 
 // Main component
 import Main from './src/components/Main';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 export default function App() {
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
       <NativeRouter>
         <Main />
       </NativeRouter>
       <StatusBar style="auto" />
-    </>
+    </ApolloProvider>
   );
 }
 
