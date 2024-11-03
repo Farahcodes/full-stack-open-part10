39dc4cae034bb8f2bdc52182f8993e556d3ce418
farahcodes <farah.fournat@gmail.com>
2024-11-03 20:56:39 +0100

    Enhancing Apollo Client's requests

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -3,12 +3,18 @@ import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
 import Constants from 'expo-constants';
+import { registerRootComponent } from 'expo';
 
 // Main component
 import Main from './src/components/Main';
+// Utils
 import createApolloClient from './src/utils/apolloClient';
+import AuthStorage from './src/utils/authStorage';
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
+
+registerRootComponent(App);
 
 export default function App() {
   console.log(Constants.expoConfig);
